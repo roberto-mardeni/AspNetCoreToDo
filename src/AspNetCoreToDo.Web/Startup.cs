5fa33388f3eaff2fe3d328b43ac514589c9bd95c
Roberto Mardeni <rmardeni@gmail.com>
2019-01-02 18:05:17 -0500

    Changed to read connection string from appsettings.json

--- src/AspNetCoreToDo.Web/Startup.cs
+++ src/AspNetCoreToDo.Web/Startup.cs
@@ -7,14 +7,30 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
 
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = @"Server=.\SQLEXPRESS2014;Database=AspNetCoreToDo;Trusted_Connection=True;";
+            //var connection = @"Server=.\SQLEXPRESS2014;Database=AspNetCoreToDo;Trusted_Connection=True;";
+            var connection = Configuration.GetConnectionString("ToDoContext");
+
             services.AddDbContext<ToDoContext>(options => options.UseSqlServer(connection));
 
             services.AddSession();
